d3adf30416b1e883fa16f41de7f3ee2932da4210
Tyler Kennedy <tk@tkte.ch>
2012-05-13 18:01:34 -0400

    Better documented and simpler descriptor parsing, while retaining more details.

--- jawa/core/descriptor.py
+++ jawa/core/descriptor.py
@@ -10,90 +10,76 @@ It does not depend on any other modules.
 
 
 class DescriptorError(Exception):
-    """
-    Raised when any generic error occurs while parsing a field or method
-    descriptor.
-    """
+    pass
 
 
 def method_descriptor(descriptor):
     """
-    Parses a method descriptor, returning a tuple in the form of
-    ``(<method parameter types>, <method return types>)``.
+    Parses a Method descriptor as described in section 4.3.3 of the JVM
+    specification. Returns the method argument types and return type.
     """
-    if descriptor[0] != '(':
-        # A method desciptor must start with its arguments, which are
-        # wrapped in brackets.
-        raise DescriptorError('No opening bracket')
+    if not descriptor.startswith('('):
+        raise DescriptorError('missing opening bracket')
+    end_para = descriptor.find(')')
+    if end_para == -1:
+        raise DescriptorError('missing closing bracket')
 
-    end = descriptor.find(')')
-    if end == -1:
-        raise DescriptorError('no terminating bracket')
+    method_args = parse_descriptor(descriptor[1:end_para])
+    return_args = parse_descriptor(descriptor[end_para + 1:])
 
-    # Parse the descriptor in two parts, the (optional) method arguments
-    # and the mandatory return type.
-    args = split_descriptor(descriptor[1:end])
-    ret = split_descriptor(descriptor[end + 1:])
-
-    # There must always be a return type, even for methods which return
-    # nothing (void).
-    if not ret:
-        raise DescriptorError('no method return type')
-
-    return args, ret[0]
+    return method_args, return_args[0]
 
 
 def field_descriptor(descriptor):
     """
-    Parses a field descriptor, returning the type of the field.
+    Parses a Field descriptor as described in section 4.3.2 of the JVM
+    specification. Returns the simple type of the field.
     """
-    return split_descriptor(descriptor)[0]
+    return parse_descriptor(descriptor)[0]
 
 
-def split_descriptor(descriptor):
+# JVM Descriptor "BaseType" characters to their
+# full simple type.
+_HUMAN_NAMES = {
+    'L': 'reference',
+    'B': 'byte',
+    'C': 'char',
+    'D': 'double',
+    'F': 'float',
+    'I': 'int',
+    'J': 'long',
+    'S': 'short',
+    'Z': 'boolean',
+    'V': 'void'
+}
+
+
+def parse_descriptor(descriptor):
     """
-    Parses a descriptor in a manner compliant with section 4.4.1 of the
-    Java5 ClassFile Format Specification.
+    Uses a tiny state machine to parse JVM descriptors. To get useful wrappers
+    around the results, use :py:method:`jawa.core.descriptor.method_descriptor`
+    or :py:method:`jawa.core.descriptor.field_descriptor`.
     """
-    d = descriptor
-    i = 0
-    ret = []
-    post = ''
-    while i < len(d):
-        # Each "[" denotes another array dimension
-        if d[i] == '[':
-            post += '[]'
-        else:
-            # Class types being with a 'L' and are terminated by a ';'.
-            if d[i] == 'L':
-                end = d.find(';', i)
-                if end == -1:
-                    raise DescriptorError('no terminating semicolon')
-                ret.append(d[i + 1:end].replace('/', '.'))
-                i = end - 1
-            elif d[i] == 'B':
-                ret.append('byte')
-            elif d[i] == 'C':
-                ret.append('char')
-            elif d[i] == 'D':
-                ret.append('double')
-            elif d[i] == 'F':
-                ret.append('float')
-            elif d[i] == 'I':
-                ret.append('int')
-            elif d[i] == 'J':
-                ret.append('long')
-            elif d[i] == 'S':
-                ret.append('short')
-            elif d[i] == 'Z':
-                ret.append('boolean')
-            elif d[i] == 'V':
-                ret.append('void')
-
-            if post:
-                ret[-1] += post
-                post = ''
-
-        i += 1
-
-    return tuple(ret)
+    # States:
+    #   10 == NORMAL,
+    #   20 == OBJECT REFERENCE
+    state = 10
+    tokens = []
+    token = []
+    dimensions = 0
+    for char in descriptor:
+        if state == 10 and char == 'L':
+            state = 20
+        elif state == 10 and char == '[':
+            dimensions += 1
+        elif state == 10:
+            tokens.append((char, dimensions, _HUMAN_NAMES[char]))
+            dimensions = 0
+        elif state == 20 and char == ';':
+            tokens.append((''.join(token), dimensions, 'reference'))
+            dimensions = 0
+            state = 10
+            del token[:]
+        elif state == 20:
+            token.append(char)
+    return tokens
