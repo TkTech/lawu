b68bb4990ed40aae8ed3a075042c93964397f6aa
Tyler Kennedy <tk@tkte.ch>
2012-05-13 18:46:25 -0400

    Moved the generic JarFile to its own file and renamed it, and implemented a Jawa-specific subclass as JarFile.

---
+++ jawa/util/ezip.py
@@ -0,0 +1,117 @@
+import re
+
+# czipfile offers a noticable improvement, especially when working with
+# many smaller files.
+try:
+    from czipfile import ZipFile
+except ImportError:
+    from zipfile import ZipFile
+
+
+class ZipPathResult(unicode):
+    """
+    A very useful helper when iterating results from a
+    :py:class:`jawa.utils.jf.JarFile` that acts like a normal string, but
+    additionally provides a ``read()`` method.
+    """
+    def __new__(self, jar, *args):
+        return unicode.__new__(self, *args)
+
+    def __init__(self, jar, *args, **kwargs):
+        super(ZipPathResult, self).__init__(*args, **kwargs)
+        self.__jar_file = jar
+
+    def read(self):
+        return self.__jar_file.read(self)
+
+
+class EditableZipFile(object):
+    """
+    A light wrapper around the python :py:class:`zipfile.ZipFile` that
+    enables in-memory modification.
+
+    If `io` is provided, its contents are loaded. `io` may be a file-system
+    path or a file-like object.
+    """
+    def __init__(self, io=None):
+        self.io = ZipFile(io, 'r') if io else None
+        self._cache = {}
+        # The ZipFile namelist() appears to re-scan on each call. Keeping track
+        # ourselves more than halves iteration times.
+        self._namelist = set(self.io.namelist() if io else ())
+        self._o_namelist = frozenset(self._namelist)
+
+    @property
+    def namelist(self):
+        """
+        Returns a list of all paths in this JarFile.
+        """
+        return self._namelist
+
+    def read(self, path):
+        """
+        Returns the contents of `path`, if it exists, otherwise `None`.
+        """
+        if path not in self._namelist:
+            return None
+        elif path in self._cache:
+            return self._cache[path]
+        elif self.io and path in self._o_namelist:
+            return self.io.read(path)
+        return None
+
+    def write(self, path, data):
+        """
+        Writes `data` to `path`, overwritting any existing content.
+        """
+        # TODO: Evaluate using tempfile.TemporarySpooledFile to better support
+        #       large files.
+        self._cache[path] = data
+        self._namelist.add(path)
+
+    def save(self, path):
+        """
+        Saves the JarFile to `path`, which can be a file-system path or a
+        file-like object.
+        """
+        zp_out = ZipFile(path, 'w')
+        for zp_name in self._namelist:
+            zp_out.writestr(zp_name, self.read(zp_name))
+        zp_out.close()
+
+    def remove(self, path):
+        """
+        Removes a file from the JarFile.
+
+        >>> print jf.namelist()
+        ['my_file.txt', 'that_other_file.txt', ...]
+        >>> jf.remove('my_file.txt')
+        >>> print jf.namelist()
+        ['that_other_file.txt', ...]
+        """
+        self._namelist.discard(path)
+        self._cache.pop(path, None)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc_info):
+        """
+        Closes the JarFile and any underlying objects.
+        """
+        if self.io:
+            self.io.close()
+
+    def regex(self, regex):
+        """
+        Returns an iterator over every path that matches `regex`. For example,
+        to get all files ending in .class that aren't in a sub-directory:
+
+        >>> for path in jf.regex('[^/]+\.class'):
+        ...    print(path)
+        """
+        for p in self._namelist:
+            if re.match(regex, p):
+                yield ZipPathResult(self, p)
+
+    close = __exit__
--- jawa/util/jf.py
+++ jawa/util/jf.py
@@ -7,40 +7,13 @@ some mandatory structure.
     If `czipfile <http://pypi.python.org/pypi/czipfile>`_ is available,
     decompression will be noticably faster.
 """
-import re
-
-# czipfile offers a noticable improvement, especially when working with
-# many smaller files.
-try:
-    from czipfile import ZipFile
-except ImportError:
-    from zipfile import ZipFile
+from jawa.util.ezip import EditableZipFile
+from jawa.core.cf import ClassFile
 
 
-class JarPathResult(unicode):
+class JarFile(EditableZipFile):
     """
-    A very useful helper when iterating results from a
-    :py:class:`jawa.utils.jf.JarFile` that acts like a normal string, but
-    additionally provides a ``read()`` method.
-    """
-    def __new__(self, jar, *args):
-        return unicode.__new__(self, *args)
-
-    def __init__(self, jar, *args, **kwargs):
-        super(JarPathResult, self).__init__(*args, **kwargs)
-        self.__jar_file = jar
-
-    def read(self):
-        return self.__jar_file.read(self)
-
-
-class JarFile(object):
-    """
-    A light wrapper around the python :py:class:`zipfile.ZipFile` that
-    enables in-memory modification.
-
-    If `io` is provided, its contents are loaded. `io` may be a file-system
-    path or a file-like object.
+    Provides Jawa-specific helpers around EditableZipFile.
 
     As of 0.1.0, :py:class:`jawa.util.jf.JarFile` is also a context manager.
 
@@ -48,84 +21,17 @@ class JarFile(object):
     >>> with JarFile(sys.argv[1]) as jf:
     ...    print(jf.namelist())
     """
-    def __init__(self, io=None):
-        self.io = ZipFile(io, 'r') if io else None
-        self._cache = {}
-        # The ZipFile namelist() appears to re-scan on each call. Keeping track
-        # ourselves more than halves iteration times.
-        self._namelist = set(self.io.namelist() if io else ())
-        self._o_namelist = frozenset(self._namelist)
+    def open_class(self, path):
+        """
+        Return's a :py:class:`jawa.core.cf.ClassFile` for the given `path`.
+        """
+        return ClassFile.from_str(self.read(path))
 
-    def namelist(self):
+    def all_classes(self):
         """
-        Returns a list of all paths in this JarFile.
+        An iterator that yields a :py:class:`jawa.core.cf.ClassFile` for each
+        path ending in `.class` in this JarFile.
         """
-        return self._namelist
-
-    def read(self, path):
-        """
-        Returns the contents of `path`, if it exists, otherwise `None`.
-        """
-        if path not in self._namelist:
-            return None
-        elif path in self._cache:
-            return self._cache[path]
-        elif self.io and path in self._o_namelist:
-            return self.io.read(path)
-        return None
-
-    def write(self, path, data):
-        """
-        Writes `data` to `path`, overwritting any existing content.
-        """
-        # TODO: Evaluate using tempfile.TemporarySpooledFile to better support
-        #       large files.
-        self._cache[path] = data
-        self._namelist.add(path)
-
-    def save(self, path):
-        """
-        Saves the JarFile to `path`, which can be a file-system path or a
-        file-like object.
-        """
-        zp_out = ZipFile(path, 'w')
-        for zp_name in self._namelist:
-            zp_out.writestr(zp_name, self.read(zp_name))
-        zp_out.close()
-
-    def remove(self, path):
-        """
-        Removes a file from the JarFile.
-
-        >>> print jf.namelist()
-        ['my_file.txt', 'that_other_file.txt', ...]
-        >>> jf.remove('my_file.txt')
-        >>> print jf.namelist()
-        ['that_other_file.txt', ...]
-        """
-        self._namelist.discard(path)
-        self._cache.pop(path, None)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *exc_info):
-        """
-        Closes the JarFile and any underlying objects.
-        """
-        if self.io:
-            self.io.close()
-
-    def regex(self, regex):
-        """
-        Returns an iterator over every path that matches `regex`. For example,
-        to get all files ending in .class that aren't in a sub-directory:
-
-        >>> for path in jf.regex('[^/]+\.class'):
-        ...    print(path)
-        """
-        for p in self._namelist:
-            if re.match(regex, p):
-                yield JarPathResult(self, p)
-
-    close = __exit__
+        # About 10ms faster for 1028 files than using self.regex()
+        for path in (p for p in self.namelist if p.endswith('.class')):
+            yield ClassFile.from_str(self.read(path))
