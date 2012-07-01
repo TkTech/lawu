2859113ffebf64e8612d5301c5eacabe34e94fe0
Tyler Kennedy <tk@tkte.ch>
2012-07-01 13:03:21 -0400

    Changed context's find_class to behave more like Java; added support for directories and .jar paths.

--- jawa/core/context.py
+++ jawa/core/context.py
@@ -2,31 +2,46 @@
 import os
 
 from jawa.core.jf import JarFile
+from jawa.core.cf import ClassFile
+
+
+class ContextPathError(Exception):
+    def __init__(self, msg, path):
+        super(ContextPathError, self).__init__(msg)
+        self.path = path
 
 
 class Context(object):
     """
     A :class:`~jawa.core.context.Context` object exists to mimic the Java
     CLASSPATH and other environment flags & settings.
+
+    There are important performance considerations to make when using
+    paths to ``.jar`` files. When doing a class lookup, paths ending in
+    ``.jar`` will be substituded with a :class:`~jawa.core.jf.JarFile` object
+    which will remain in memory until removed.
     """
     def __init__(self):
-        self._classpath = set()
+        self._classpath = []
 
     @property
     def classpath(self):
         """
-        Returns a list of the currently active classpaths, which may be a
-        filesystem path (as a str) or a :class:`~jawa.core.jf.JarFile`.
+        An iterator over the currently active classpaths.
         """
-        return list(self._classpath)
+        return iter(self._classpath)
 
     def add_classpath(self, path_or_jar):
         """
         Adds a new classpath to the current
-        :class:`~jawa.core.context.Context`. The path may be a literal
-        filesystem path, or an in-memory :class:`~jawa.core.jf.JarFile`.
+        :class:`~jawa.core.context.Context`. The path may be a:
+
+        * filesystem path to a ``.jar`` file,
+        * filesystem path to a directory,
+        * an in-memory :class:`~jawa.core.jf.JarFile`.
         """
-        self._classpath.add(path_or_jar)
+        if path_or_jar not in self._classpath:
+            self._classpath.append(path_or_jar)
 
     def find_class(self, class_, no_inherit=False):
         """
@@ -37,17 +52,22 @@ class Context(object):
         if not class_.endswith('.class'):
             class_ = '{}.class'.format(class_)
 
-        for path in self.classpath:
+        for i, path in enumerate(self._classpath):
             if isinstance(path, JarFile):
                 if class_ in path.namelist:
                     return path.open_class(
                         class_, context=None if no_inherit else self)
-                continue
             elif os.path.isfile(path) and path.endswith('.jar'):
-                raise NotImplementedError()
-            elif os.path.isfile(path):
-                raise ValueError('not a directory or a JAR')
+                self._classpath[i] = JarFile(path)
+                return self.find_class(class_, no_inherit=no_inherit)
             elif os.path.isdir(path):
-                raise NotImplementedError()
+                final_path = class_.replace('/', os.sep)
+                final_path = os.join(path, final_path)
+                if os.path.isfile(final_path):
+                    return ClassFile(final_path)
+            else:
+                raise ContextPathError(
+                    'path was not a .jar or a directory', path)
 
+        # We couldn't find the class.
         return None
