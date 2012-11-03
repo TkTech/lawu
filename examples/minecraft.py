56e2d87a81a66fac6e2b85e2e2b3297171daee11
Tyler Kennedy <tk@tkte.ch>
2012-11-03 07:27:52 -0400

    Fixing example

--- examples/minecraft.py
+++ examples/minecraft.py
@@ -10,11 +10,8 @@ from jawa import JarFile
 if __name__ == '__main__':
     with JarFile(sys.argv[1]) as jf:
         for path, cf in jf.all_classes():
-            for method in cf.methods.find(f=lambda m: m.code):
-                for ins in method.code.disassemble():
-                    print ins
 
-            # Do you modifications here.
+            # Do your modifications here.
 
             with jf.open(path, 'w') as fout:
                 cf.save(fout)
