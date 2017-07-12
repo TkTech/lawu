53874cfbdc55d79dc7287aedc79ae0a7745a87fe
Tyler Kennedy <tk@tkte.ch>
2017-07-12 00:49:57 -0400

    Move ClassFile loading to fixtures in tests.

--- tests/test_stack_map_attribute.py
+++ tests/test_stack_map_attribute.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import os.path
 
+import pytest
+
 from jawa import ClassFile
 from jawa.util.verifier import VerificationTypes
 
 
-def test_stack_map_table_read():
+@pytest.fixture
+def cf():
     sample_path = os.path.join(
         os.path.dirname(__file__),
         'data',
@@ -14,23 +17,26 @@ def test_stack_map_table_read():
 
     with open(sample_path, 'rb') as fin:
         cf = ClassFile(fin)
+        yield cf
 
-        m = cf.methods.find_one(name='addOne')
-        a = m.code.attributes.find_one(name='StackMapTable')
 
-        assert len(a.frames) == 2
+def test_stack_map_table_read(cf):
+    m = cf.methods.find_one(name='addOne')
+    a = m.code.attributes.find_one(name='StackMapTable')
 
-        assert a.frames[0].frame_type == 76
-        assert a.frames[0].frame_stack == [
-            (VerificationTypes.ITEM_Integer,)
-        ]
+    assert len(a.frames) == 2
 
-        assert a.frames[1].frame_type == 255
-        assert a.frames[1].frame_stack == [
-            (VerificationTypes.ITEM_Integer,),
-            (VerificationTypes.ITEM_Integer,)
-        ]
-        assert a.frames[1].frame_locals == [
-            (VerificationTypes.ITEM_Object, 17),
-            (VerificationTypes.ITEM_Integer,)
-        ]
+    assert a.frames[0].frame_type == 76
+    assert a.frames[0].frame_stack == [
+        (VerificationTypes.ITEM_Integer,)
+    ]
+
+    assert a.frames[1].frame_type == 255
+    assert a.frames[1].frame_stack == [
+        (VerificationTypes.ITEM_Integer,),
+        (VerificationTypes.ITEM_Integer,)
+    ]
+    assert a.frames[1].frame_locals == [
+        (VerificationTypes.ITEM_Object, 17),
+        (VerificationTypes.ITEM_Integer,)
+    ]
