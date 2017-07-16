TkTech/lawu

commit d82b4d0175919c292e14b391cfb4b7335ef12f6c

Formatting fix

jawa/constants.py
@@ -576,25 +576,25 @@ class ConstantPool(object):
                     constant._descriptor_index
                 ))
             elif isinstance(constant, ConstantMethodHandle):
-                            write(pack(
-                                '>BBH',
-                                constant.TAG,
-                                constant._reference_kind,
-                                constant._reference_index
-                            ))
+                write(pack(
+                    '>BBH',
+                    constant.TAG,
+                    constant._reference_kind,
+                    constant._reference_index
+                ))
             elif isinstance(constant, ConstantMethodType):
-                            write(pack(
-                                '>BH',
-                                constant.TAG,
-                                constant._descriptor_index
-                            ))
+                write(pack(
+                    '>BH',
+                    constant.TAG,
+                    constant._descriptor_index
+                ))
             elif isinstance(constant, ConstantInvokeDynamic):
-                            write(pack(
-                                '>BHH',
-                                constant.TAG,
-                                constant._bootstrap_method_attr_index,
-                                constant._name_and_type_index
-                            ))
+                write(pack(
+                    '>BHH',
+                    constant.TAG,
+                    constant._bootstrap_method_attr_index,
+                    constant._name_and_type_index
+                ))
 
     def __len__(self):
         """

commit 9b8589fba298f0a2f965f476935920b4984e7a0d

Fix PEP8 errors introduced in d82b4d01

jawa/constants.py
@@ -1,24 +1,4 @@
 # -*- coding: utf8 -*-
-__all__ = (
-    'ConstantPool',
-    'Constant',
-    'ConstantUTF8',
-    'ConstantClass',
-    'ConstantString',
-    'ConstantRef',
-    'ConstantFieldRef',
-    'ConstantMethodRef',
-    'ConstantInterfaceMethodRef',
-    'ConstantInteger',
-    'ConstantFloat',
-    'ConstantLong',
-    'ConstantDouble',
-    'ConstantNameAndType',
-    'ConstantMethodHandle',
-    'ConstantMethodType',
-    'ConstantInvokeDynamic',
-)
-
 from struct import unpack, pack
 
 from jawa.util.utf import decode_modified_utf8, encode_modified_utf8
@@ -164,6 +144,7 @@ class ConstantNameAndType(Constant):
         return 'ConstantNameAndType(name={0!r}, descriptor={1!r})'.format(
             self.name, self.descriptor)
 
+
 class ConstantMethodHandle(Constant):
     TAG = 15
 
@@ -184,6 +165,7 @@ class ConstantMethodHandle(Constant):
         return 'ConstantMethodHandle(kind={0!r}, index={1!r})'.format(
             self.kind, self.reference)
 
+
 class ConstantMethodType(Constant):
     TAG = 16
 
@@ -199,10 +181,12 @@ class ConstantMethodType(Constant):
         return 'ConstantMethodType(descriptor={0!r})'.format(
             self.descriptor)
 
+
 class ConstantInvokeDynamic(Constant):
     TAG = 18
 
-    def __init__(self, pool, index, bootstrap_method_attr_index, name_and_type_index):
+    def __init__(self, pool, index, bootstrap_method_attr_index,
+                 name_and_type_index):
         super(ConstantInvokeDynamic, self).__init__(pool, index)
         self._bootstrap_method_attr_index = bootstrap_method_attr_index
         self._name_and_type_index = name_and_type_index
@@ -216,8 +200,10 @@ class ConstantInvokeDynamic(Constant):
         return self.pool.get(self._name_and_type_index)
 
     def __repr__(self):
-        return 'ConstantInvokeDynamic(methodAttr={0!r}, nameAndType={1!r})'.format(
-            self.methodAttr, self.nameAndType)
+        return (
+            'ConstantInvokeDynamic(methodAttr={0!r}, '
+            'nameAndType={1!r})'
+        ).format(self.methodAttr, self.nameAndType)
 
 _constant_types = (
     None,