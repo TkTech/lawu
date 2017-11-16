65321956cc0466413285e99f8254fb7e18f46b97
Tyler Kennedy <tk@tkte.ch>
2017-11-16 17:54:53 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="jawa",
     packages=find_packages(),
-    version="1.0",
+    version="1.1",
     description="Doing fun stuff with JVM ClassFiles.",
     author="Tyler Kennedy",
     author_email="tk@tkte.ch",
