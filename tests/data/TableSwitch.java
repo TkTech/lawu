f9c51fc6c90f2b7e34c5f025da91b4e6c469a0e7
Tyler Kennedy <tk@tkte.ch>
2018-05-05 15:59:23 -0400

    Add a TableSwitch.class and TableSwitch.java with a TableSwitch opcode.

---
+++ tests/data/TableSwitch.java
@@ -0,0 +1,11 @@
+/* *Most* compilers should generate a tableswitch from this */
+class TableSwitch {
+    public static void main(String[] args) {
+        switch(1) {
+            case 1: return;
+            case 2: return;
+            case 3: return;
+            default: return;
+        }
+    }
+}
