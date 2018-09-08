ae20f32e4a44df9fd62edbb3bd7d005b2e616bd6
Tyler Kennedy <tk@tkte.ch>
2018-09-08 00:31:38 -0400

    Preserve end_of_line tokens in the tokenizer, always yield an EOL at the end of tokenizer(). Necessary for parsing some Jasmin statements.

--- jawa/jasmin/tokenizer.py
+++ jawa/jasmin/tokenizer.py
@@ -2,20 +2,21 @@ from enum import Enum
 from typing import Any, TextIO, Iterator, Union
 
 
-class States(Enum):
+class TokenType(Enum):
     GENERIC = 1
     QUOTED_STRING = 2
     DIRECTIVE = 3
     COMMENT = 4
     LITERAL = 5
     QUOTED_ESCAPE = 6
+    END_OF_LINE = 7
 
 
 class Token(object):
     __slots__ = ('token_type', 'value', 'line_no')
 
     def __init__(self, *, token_type=None, value=None, line_no=None):
-        self.token_type: int = token_type or States.GENERIC
+        self.token_type: int = token_type or TokenType.GENERIC
         self.value: Any = value
         self.line_no: Union[int, None] = line_no
 
@@ -34,60 +35,75 @@ def tokenize(source: TextIO) -> Iterator[Token]:
     :param source: Source to read from.
     :return: Iterator of Token objects.
     """
-    s = States.GENERIC
+    s = TokenType.GENERIC
     c = source.read(1)
     prev_c = ''
     buff = []
     line_no = 1
     while c != '':
-        if s in (States.GENERIC, States.DIRECTIVE):
-            # Start of a directive.
+        if s in (TokenType.GENERIC, TokenType.DIRECTIVE):
             if c == '.':
-                s = States.DIRECTIVE
+                # We've found the start of a directive.
+                s = TokenType.DIRECTIVE
             elif c == ';' and prev_c in (' ', '\t', '\n', ''):
-                s = States.COMMENT
+                # We've found the start of a comment.
+                s = TokenType.COMMENT
             elif c in (' ', '\t', '\n'):
+                # We've found some whitespace, which acts as a terminator
+                # in Jasmin. We don't care about blanks, so we only yield
+                # if there's something in the buffer.
                 value = ''.join(buff)
                 if value:
                     yield Token(token_type=s, value=value, line_no=line_no)
-                s = States.GENERIC
+                s = TokenType.GENERIC
                 del buff[:]
             elif c in '"':
-                s = States.QUOTED_STRING
+                # We've found the start of a quoted string and need to handle
+                # escapes.
+                s = TokenType.QUOTED_STRING
             else:
                 buff.append(c)
-        elif s == States.COMMENT:
+        elif s == TokenType.COMMENT:
             if c == '\n':
                 value = ''.join(buff)
                 if value:
                     yield Token(token_type=s, value=value, line_no=line_no)
-                s = States.GENERIC
+                s = TokenType.GENERIC
                 del buff[:]
             elif not buff and c in (' ', '\t'):
                 # Skip starting whitespace on comments.
                 pass
             else:
                 buff.append(c)
-        elif s == States.QUOTED_STRING:
+        elif s == TokenType.QUOTED_STRING:
             if c == '\\':
-                s = States.QUOTED_ESCAPE
+                # We've found the start of an escape, such as \".
+                s = TokenType.QUOTED_ESCAPE
             elif c == '"':
+                # We've found the end of the quoted string.
                 yield Token(token_type=s, value=''.join(buff), line_no=line_no)
-                s = States.GENERIC
+                s = TokenType.GENERIC
                 del buff[:]
             else:
                 buff.append(c)
-        elif s == States.QUOTED_ESCAPE:
-            s = States.QUOTED_STRING
+        elif s == TokenType.QUOTED_ESCAPE:
+            s = TokenType.QUOTED_STRING
             buff.append(c)
             if c == '\\':
-                s = States.QUOTED_ESCAPE
+                s = TokenType.QUOTED_ESCAPE
 
         if c == '\n':
+            yield Token(token_type=TokenType.END_OF_LINE, line_no=line_no)
+            # We keep track of what line we're currently tokenizing to use
+            # later for useful error messages.
             line_no += 1
 
         prev_c = c
         c = source.read(1)
 
+    # If tokenizing a subset, or if a file is missing the terminating
+    # newline, then we yield whatever is leftover in the buffer. Probably
+    # not correct.
     if buff:
         yield Token(token_type=s, value=''.join(buff), line_no=line_no)
+    yield Token(token_type=TokenType.END_OF_LINE, line_no=line_no)
--- tests/jasmin/test_tokenizer.py
+++ tests/jasmin/test_tokenizer.py
@@ -1,59 +1,69 @@
 from io import StringIO
 
-from jawa.jasmin.tokenizer import tokenize, States
+from jawa.jasmin.tokenizer import tokenize, TokenType
 
 
 def test_quoted_string():
     tokens = list(tokenize(
         StringIO('"No Escapes"')
     ))
-    assert len(tokens) == 1
-    assert tokens[0].token_type == States.QUOTED_STRING
+    assert len(tokens) == 2
+    assert tokens[0].token_type == TokenType.QUOTED_STRING
     assert tokens[0].value == 'No Escapes'
     assert tokens[0].line_no == 1
+
+    assert tokens[1].token_type == TokenType.END_OF_LINE
 
     tokens = list(tokenize(
         StringIO('"Escaped \\"Quote"')
     ))
-    assert len(tokens) == 1
-    assert tokens[0].token_type == States.QUOTED_STRING
+    assert len(tokens) == 2
+    assert tokens[0].token_type == TokenType.QUOTED_STRING
     assert tokens[0].value == 'Escaped \"Quote'
     assert tokens[0].line_no == 1
+
+    assert tokens[1].token_type == TokenType.END_OF_LINE
 
 
 def test_directive():
     tokens = list(tokenize(
         StringIO('.class public HelloWorld')
     ))
-    assert len(tokens) == 3
-    assert tokens[0].token_type == States.DIRECTIVE
+    assert len(tokens) == 4
+    assert tokens[0].token_type == TokenType.DIRECTIVE
     assert tokens[0].value == 'class'
     assert tokens[0].line_no == 1
 
-    assert tokens[1].token_type == States.GENERIC
+    assert tokens[1].token_type == TokenType.GENERIC
     assert tokens[1].value == 'public'
     assert tokens[1].line_no == 1
 
-    assert tokens[2].token_type == States.GENERIC
+    assert tokens[2].token_type == TokenType.GENERIC
     assert tokens[2].value == 'HelloWorld'
     assert tokens[2].line_no == 1
+
+    assert tokens[3].token_type == TokenType.END_OF_LINE
 
 
 def test_comment():
     tokens = list(tokenize(
         StringIO('; Test Comment')
     ))
-    assert len(tokens) == 1
-    assert tokens[0].token_type == States.COMMENT
+    assert len(tokens) == 2
+    assert tokens[0].token_type == TokenType.COMMENT
     assert tokens[0].value == 'Test Comment'
     assert tokens[0].line_no == 1
+
+    assert tokens[1].token_type == TokenType.END_OF_LINE
 
 
 def test_semicolon_in_generic():
     tokens = list(tokenize(
         StringIO('abc;123')
     ))
-    assert len(tokens) == 1
-    assert tokens[0].token_type == States.GENERIC
+    assert len(tokens) == 2
+    assert tokens[0].token_type == TokenType.GENERIC
     assert tokens[0].value == 'abc;123'
     assert tokens[0].line_no == 1
+
+    assert tokens[1].token_type == TokenType.END_OF_LINE
